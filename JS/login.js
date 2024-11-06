a37b6901fedc68063ccdafbd89f81d67ebf34b91
Ajay Negi <ajay.negi@CELSYSWTC.IN>
2024-11-06 12:16:59 +0530

    Login Page

---
+++ JS/login.js
@@ -0,0 +1,20 @@
+
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+
+    
+    const userData = JSON.parse(localStorage.getItem("user"));
+
+    
+    if (userData && userData.email === email && userData.password === password) {
+        // Successfully logged in
+        alert("Login successful!");
+        window.location.href = "home.html"; 
+    } else {
+        alert("Invalid email or password!");
+    }
+});
